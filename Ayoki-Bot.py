cf1e02afeb74765f0f7af94d3857ea845f31dd7c
chamathdissanayaka <87513375+chamathdissanayaka@users.noreply.github.com>
2021-11-24 17:36:08 +0530

    .

--- Ayoki-Bot.py
+++ Ayoki-Bot.py
@@ -39,4 +39,4 @@ exec(compile(ast.parse(patched), "<string>", "exec"),
 discord.gateway.DiscordWebSocket.identify = loc["identify"]
 
 
-bot.run("token")
+bot.run("OTEyODk5MDA2OTkxMDY1MTQ4.YZ2pdA.CAQPvXpNLmwmkBcMgyhg4td5K6I")
