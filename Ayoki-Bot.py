82ce1a3659faa42b98208b934206374118d0a96d
chamathdissanayaka <87513375+chamathdissanayaka@users.noreply.github.com>
2021-11-24 20:10:35 +0530

    removed one cog

--- Ayoki-Bot.py
+++ Ayoki-Bot.py
@@ -2,7 +2,7 @@ import discord
 import inspect
 import re
 import ast
-import os
+import os, aiohttp, random, asyncio, json
 from discord.ext import commands
 
 
@@ -14,6 +14,28 @@ bot = commands.Bot(command_prefix='<', intents=intents)
 async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="<help for Cuddles💞"))
     print('cutee Ayoki online!!')
+@bot.command(pass_context=True, help='send Gifs', categories=': DO `<` `gif name`')
+async def gif(ctx, *, search):
+    async with ctx.typing():
+        await asyncio.sleep(0.1)
+    embed = discord.Embed(colour=discord.Colour.magenta())
+    session = aiohttp.ClientSession()
+
+    if search == '':
+        response = await session.get('https://api.giphy.com/v1/gifs/random?api_key=vBFb7XWFdnbCNn3Atf9FBbQ68XG4EoV9')
+        data = json.loads(await response.text())
+        embed.set_image(url=data['data']['images']['original']['url'])
+    else:
+        search.replace(' ', '+')
+        response = await session.get('http://api.giphy.com/v1/gifs/search?q=' + search + '&api_key=vBFb7XWFdnbCNn3Atf9FBbQ68XG4EoV9&limit=10')
+        data = json.loads(await response.text())
+        gif_choice = random.randint(0, 9)
+        embed.set_image(url=data['data'][gif_choice]
+                        ['images']['original']['url'])
+
+    await session.close()
+
+    await ctx.send(embed=embed)
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
